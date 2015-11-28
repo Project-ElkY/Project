f189f40c9e169d70d6919474948a9bf0852271b5
tcholakov <tcholakov2@gmail.com>
2015-11-28 17:01:43 +0200

    Added ElkyPlayerStrategy

--- Logic/AI/ElkYPlayer.cs
+++ Logic/AI/ElkYPlayer.cs
@@ -1,20 +1,26 @@
 ﻿namespace TexasHoldem.AI
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Helpers;
-    using TexasHoldem.AI;
-    using TexasHoldem.Logic;
     using TexasHoldem.Logic.Players;
+    using global::AI.Strategy;
 
     public class ElkYPlayer : BasePlayer
     {
+        private IElkyPlayerStrategy strategy;
+
+        public ElkYPlayer(IElkyPlayerStrategy strategy)
+            :base()
+        {
+            this.strategy = strategy;
+        }
+
+        public ElkYPlayer()
+            : this(new ElkyPlayerStrategy())
+        { }
+
         public override string Name { get; } = "ElkYPlayer";
         
         public override void StartGame(StartGameContext context)
         {
-            InitMyGameStartegy();
             base.StartGame(context);
         }
         
@@ -30,126 +36,7 @@
         
         public override PlayerAction GetTurn(GetTurnContext context)
         {
-            if (context.RoundType == GameRoundType.PreFlop)
-            {
-                PlayerAction act = PreFlopAction(context);
-                return act;
-            }
-
-            if (context.RoundType == GameRoundType.Flop || 
-                context.RoundType == GameRoundType.River || 
-                context.RoundType == GameRoundType.Turn)
-            {
-                PlayerAction act = FlopAction(context);
-                return act;                
-            }
-
-            return PlayerAction.CheckOrCall();
-        }
-
-        private void InitMyGameStartegy()
-        {
-            MyGameStartegy.Fold = 45;
-            MyGameStartegy.Call = 70;
-            MyGameStartegy.Rise = 80;
-        }
-
-        private PlayerAction FlopAction(GetTurnContext context)
-        {
-            var playerFirstHand = ParseHandToString.GenerateStringFromCard(this.FirstCard);
-            var playerSecondHand = ParseHandToString.GenerateStringFromCard(this.SecondCard);
-
-            string playerHand = playerFirstHand + " " + playerSecondHand;
-            string openCards = string.Empty;
-
-            foreach (var item in this.CommunityCards)
-            {
-                openCards += ParseHandToString.GenerateStringFromCard(item) + " ";
-            }
-
-            var chance = MonteCarloAnalysis.CalculateWinChance(playerHand, openCards.Trim());
-
-            if (chance < MyGameStartegy.Fold)
-            {
-                return PlayerAction.Fold();
-            }
-            else if (chance < MyGameStartegy.Call)
-            {
-                return PlayerAction.CheckOrCall();
-            }
-            else if (chance < MyGameStartegy.Rise)
-            {
-                if (context.SmallBlind * 2 < context.MoneyLeft)
-                {
-                    return PlayerAction.Raise(context.SmallBlind * 2);
-                }
-                else
-                {
-                    int putMoney = context.MoneyLeft;
-
-                    if (putMoney != 0)
-                    {
-                        return PlayerAction.Raise(putMoney);
-                    }
-
-                    return PlayerAction.CheckOrCall();
-                }
-            }
-            else
-            {
-                int putMoney = context.MoneyLeft;
-
-                if (putMoney != 0)
-                {
-                    return PlayerAction.Raise(putMoney);
-                }
-
-                return PlayerAction.CheckOrCall();
-            }
-        }
-
-        private PlayerAction PreFlopAction(GetTurnContext context)
-        {
-
-            var playHand = InitialHandEvaluation.PreFlop(this.FirstCard, this.SecondCard);
-
-            if (playHand < MyGameStartegy.Fold)
-            {
-                return PlayerAction.Fold();
-            }
-            else if (playHand < MyGameStartegy.Call)
-            {
-                return PlayerAction.CheckOrCall();
-            }
-            else if (playHand < MyGameStartegy.Rise)
-            {
-                if (context.SmallBlind * 2 < context.MoneyLeft)
-                {
-                    return PlayerAction.Raise(context.SmallBlind * 2);
-                }
-                else
-                {
-                    int putMoney = context.MoneyLeft;
-
-                    if (putMoney != 0)
-                    {
-                        return PlayerAction.Raise(putMoney);
-                    }
-
-                    return PlayerAction.CheckOrCall();
-                }
-            }
-            else
-            {
-                int putMoney = context.MoneyLeft;
-
-                if (putMoney != 0)
-                {
-                    return PlayerAction.Raise(putMoney);
-                }
-
-                return PlayerAction.CheckOrCall();
-            }
+            return strategy.MakeTurn(context, this.FirstCard, this.SecondCard, this.CommunityCards);
         }
     }
 }
---
+++ Logic/AI/Strategy/ElkyPlayerStrategy.cs
@@ -0,0 +1,145 @@
+﻿namespace AI.Strategy
+{
+    using TexasHoldem.AI.Helpers;
+    using TexasHoldem.Logic.Players;
+    using TexasHoldem.Logic.Cards;
+    using System.Collections.Generic;
+    using TexasHoldem.Logic;
+
+    public class ElkyPlayerStrategy : IElkyPlayerStrategy
+    {
+        public ElkyPlayerStrategy(int fold, int call, int raise)
+        {
+            this.Fold = fold;
+            this.Call = call;
+            this.Raise = raise;
+        }
+
+        public ElkyPlayerStrategy()
+            :this(45, 70, 80)
+        { }
+
+        public int Fold { get; set; }
+
+        public int Call { get; set; }
+
+        public int Raise { get; set; }
+
+        public PlayerAction MakeTurn(GetTurnContext context, Card firstCard, Card secondCard, IReadOnlyCollection<Card> communityCards)
+        {
+            if (context.RoundType == GameRoundType.PreFlop)
+            {
+                PlayerAction act = PreFlopAction(context, firstCard, secondCard);
+                return act;
+            }
+
+            if (context.RoundType == GameRoundType.Flop ||
+                context.RoundType == GameRoundType.River ||
+                context.RoundType == GameRoundType.Turn)
+            {
+                PlayerAction act = FlopAction(context, firstCard, secondCard, communityCards);
+                return act;
+            }
+
+            return PlayerAction.CheckOrCall();
+        }
+        
+        private PlayerAction FlopAction(GetTurnContext context, Card firstCard, Card secondCard, IReadOnlyCollection<Card> communityCards)
+        {
+            var playerFirstHand = ParseHandToString.GenerateStringFromCard(firstCard);
+            var playerSecondHand = ParseHandToString.GenerateStringFromCard(secondCard);
+
+            string playerHand = playerFirstHand + " " + playerSecondHand;
+            string openCards = string.Empty;
+
+            foreach (var item in communityCards)
+            {
+                openCards += ParseHandToString.GenerateStringFromCard(item) + " ";
+            }
+
+            var chance = MonteCarloAnalysis.CalculateWinChance(playerHand, openCards.Trim());
+
+            if (chance < this.Fold)
+            {
+                return PlayerAction.Fold();
+            }
+            else if (chance < this.Call)
+            {
+                return PlayerAction.CheckOrCall();
+            }
+            else if (chance < this.Raise)
+            {
+                if (context.SmallBlind * 2 < context.MoneyLeft)
+                {
+                    return PlayerAction.Raise(context.SmallBlind * 2);
+                }
+                else
+                {
+                    int putMoney = context.MoneyLeft;
+
+                    if (putMoney != 0)
+                    {
+                        return PlayerAction.Raise(putMoney);
+                    }
+
+                    return PlayerAction.CheckOrCall();
+                }
+            }
+            else
+            {
+                int putMoney = context.MoneyLeft;
+
+                if (putMoney != 0)
+                {
+                    return PlayerAction.Raise(putMoney);
+                }
+
+                return PlayerAction.CheckOrCall();
+            }
+        }
+
+        private PlayerAction PreFlopAction(GetTurnContext context, Card firstCard, Card secondCard)
+        {
+
+            var playHand = InitialHandEvaluation.PreFlop(firstCard, secondCard);
+
+            if (playHand < this.Fold)
+            {
+                return PlayerAction.Fold();
+            }
+            else if (playHand < this.Call)
+            {
+                return PlayerAction.CheckOrCall();
+            }
+            else if (playHand < this.Raise)
+            {
+                if (context.SmallBlind * 2 < context.MoneyLeft)
+                {
+                    return PlayerAction.Raise(context.SmallBlind * 2);
+                }
+                else
+                {
+                    int putMoney = context.MoneyLeft;
+
+                    if (putMoney != 0)
+                    {
+                        return PlayerAction.Raise(putMoney);
+                    }
+
+                    return PlayerAction.CheckOrCall();
+                }
+            }
+            else
+            {
+                int putMoney = context.MoneyLeft;
+
+                if (putMoney != 0)
+                {
+                    return PlayerAction.Raise(putMoney);
+                }
+
+                return PlayerAction.CheckOrCall();
+            }
+        }
+    }
+}
