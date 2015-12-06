b1ec903581f72e66259615337eaf6b57eb001736
Chavdar Angelov <c.l.angelov@gmail.com>
2015-12-06 20:38:20 +0200

    ElkYPlayer xml

--- Logic/AI/ElkYPlayer.cs
+++ Logic/AI/ElkYPlayer.cs
@@ -6,12 +6,25 @@
     using Helpers;
     using System;
 
+    /// <summary>
+    /// An instance of an AI for a C# Texas Hold'em game.
+    /// </summary>
     public class ElkYPlayer : BasePlayer
     {
-
+        /// <summary>
+        /// May be used for gathering information about the opposition player.
+        /// </summary>
         private IList<double> opponentStrenghtList;
+
+        /// <summary>
+        /// Holds the current player startegy.
+        /// </summary>
         private IElkyPlayerStrategy strategy;
 
+        /// <summary>
+        /// Constructor with the class dependencies.
+        /// </summary>
+        /// <param name="strategy">The player startegy for the game.</param>
         public ElkYPlayer(IElkyPlayerStrategy strategy)
             : base()
         {
@@ -19,18 +32,32 @@
             this.opponentStrenghtList = new List<double>();
         }
 
+        /// <summary>
+        /// An empty constructor to be used from the game engine.
+        /// </summary>
         public ElkYPlayer()
             : this(new ElkyPlayerStrategy())
         {
         }
 
+        /// <summary>
+        /// The player Name.
+        /// </summary>
         public override string Name { get; } = "ElkYPlayer" + Guid.NewGuid();
 
+        /// <summary>
+        /// Method used at the start of the game.
+        /// </summary>
+        /// <param name="context">The current context of the game.</param>
         public override void StartGame(StartGameContext context)
         {
             base.StartGame(context);
         }
 
+        /// <summary>
+        /// Method that gathers information about the opposition player hand strength.
+        /// </summary>
+        /// <param name="context">The current context of the game.</param>
         public override void EndHand(EndHandContext context)
         {
             /*
@@ -58,12 +85,19 @@
             base.EndHand(context);
         }
 
-
+        /// <summary>
+        /// Method used at the end of each round.
+        /// </summary>
+        /// <param name="context">The current context of the game.</param>
         public override void EndRound(EndRoundContext context)
         {
             base.EndRound(context);
         }
 
+        /// <summary>
+        /// The method gathers statistical information about the given AI vs AI battle.
+        /// </summary>
+        /// <param name="context">The current context of the game.</param>
         public override void EndGame(EndGameContext context)
         {
             if (context.WinnerName == this.Name)
@@ -85,16 +119,28 @@
             base.EndGame(context);
         }
 
+        /// <summary>
+        /// Method used at the start of the game.
+        /// </summary>
+        /// <param name="context">The current context of the game.</param>
         public override void StartHand(StartHandContext context)
         {
             base.StartHand(context);
         }
 
+        /// <summary>
+        /// Method used at the start of each round.
+        /// </summary>
+        /// <param name="context">The current context of the game.</param>
         public override void StartRound(StartRoundContext context)
         {
             base.StartRound(context);
         }
 
+        /// <summary>
+        /// The player strategy will determine what type of action to be taken.
+        /// </summary>
+        /// <param name="context">The current context of the game.</param>
         public override PlayerAction GetTurn(GetTurnContext context)
         {
             return this.strategy.MakeTurn(context, this.FirstCard, this.SecondCard, this.CommunityCards);
